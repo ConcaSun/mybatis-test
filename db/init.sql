b6f4367eb68cec4f28e3eabedf2135316d18aae7
suntg <suntugui@qq.com>
2019-04-16 18:11:01 +0800

    feat: 增加表创建语句

--- db/init.sql
+++ db/init.sql
@@ -1,11 +1,39 @@
 -- user
-create table user(
+create table t_user(
   id int primary key auto_increment,
   name varchar(100) not NULL DEFAULT '' ,
   tel  varchar(50) not null DEFAULT '',
   email varchar(100) not null DEFAULT '',
   password varchar(255) not null DEFAULT '',
   salt varchar(255) not null DEFAULT '',
+  create_time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP,
+  update_time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP
+);
+
+-- enterprise
+create table t_enterprise(
+  id int primary key auto_increment,
+  name varchar(100) not NULL DEFAULT '' ,
+  address  varchar(500) not null DEFAULT '',
+  logo varchar(255) not null DEFAULT '',
+  create_time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP,
+  update_time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP
+);
+
+-- user_enterprise
+create table t_user_enterprise(
+  user_id int not null,
+  enterprise_id INT NOT NULL ,
+  create_time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (user_id, enterprise_id)
+);
+
+-- bank_account
+create table t_bank_account(
+  id int primary key auto_increment,
+  code varchar(50) not NULL DEFAULT '' ,
+  user_id INT not null DEFAULT 0,
+  amount INT(11) not null DEFAULT 0,
   create_time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP,
   update_time TIMESTAMP not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP
 );
